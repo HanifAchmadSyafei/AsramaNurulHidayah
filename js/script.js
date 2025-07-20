f18649e2b9008b821198aa35613be680a65b15fd
HanifAchmadSyafei <hanifbna4@gmail.com>
2025-07-20 17:00:47 +0700

    Ubah tampilan lagi

--- js/script.js
+++ js/script.js
@@ -91,3 +91,17 @@ window.onclick = (e) => {
 
     window.open(`https://wa.me/${noAdmin}?text=${pesan}`, "_blank");
   };
+
+  // Scroll smooth manual (opsional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
