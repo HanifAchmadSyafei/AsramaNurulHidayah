73f8cf831e5222e64cd33f0168fa169531f8ae13
HanifAchmadSyafei <hanifbna4@gmail.com>
2025-07-20 20:43:37 +0700

    Ubah CSS dan JS

--- js/script.js
+++ js/script.js
@@ -93,15 +93,15 @@ window.onclick = (e) => {
   };
 
   // Scroll smooth manual (opsional)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    }
-  });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     if (target) {
+//       target.scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'start'
+//       });
+//     }
+//   });
+// });
