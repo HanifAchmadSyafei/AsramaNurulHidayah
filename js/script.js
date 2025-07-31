24feb8d51d98e78b9e4fce63eb568175b0895044
HanifAchmadSyafei <hanifbna4@gmail.com>
2025-07-31 20:56:56 +0700

    Ubah Tampilan HTML,CSS,JS

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ window.onclick = (e) => {
     const telepon = document.getElementById("telepon").value;
 
     const pesan = `Assalamualaikum Admin Asrama Nurul Hidayah
-    %0A%0ANama: ${nama}%0AEmail: ${email}%0ANomor HP: ${telepon}`;
+    %0A%0ANama: ${nama}%0AEmail: ${email}%0ANomor HP: ${telepon}` `Saya ingin konsultasi min`;
 
     const noAdmin = "6283872261184"; // GANTI dengan nomor admin tanpa +, awali dengan 62
 
